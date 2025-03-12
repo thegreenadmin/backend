27c00349a38f8822c809e731885eeb6985c8f8b8
Sukhdev Singh <sukhdevsingh@previnci.net>
2025-03-12 11:24:41 +0530

    updated server.js

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const corsOptions = {
 
 let app = express();
 
-app.use((req, res, next) => { req.request_time = moment().utc().toDate();next(); });
+app.use((req, res, next) => { 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  req.request_time = moment().utc().toDate();next(); 
+});
 app.use(cors(corsOptions));
 app.use(express.static('views/dist/adminlte/'));
 app.set('view engine', 'ejs');
