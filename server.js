f254d3ae77e78506c494d77c61de367af48778ee
Sukhdev Singh <sukhdevsingh@previnci.net>
2025-03-27 21:47:16 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS Beanstalk!");
+  res.send("Hello from AWS Beanstalk updatedS!");
 });
 
 const PORT = process.env.PORT || 8080;
