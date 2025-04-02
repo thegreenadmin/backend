78fa33faf22ced7fbbf9f0e97ee779e317e2c9cb
Sukhdev Singh <sukhdevsingh@previnci.net>
2025-04-02 10:18:55 +0530

    Resolved

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.use(cors());
 
 
 
-console.log("check code updated")
+console.log("code uploaded today")
 app.use((req, res, next) => { 
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   req.request_time = moment().utc().toDate();next(); 
 });
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 //app.use(cors());  // Allow all origins
 // app.use(express.static('views/dist/adminlte/'));
 // app.set('view engine', 'ejs');
